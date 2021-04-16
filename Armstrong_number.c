585a49af13eea04bb73a02626f75668e70c77290
Sujan Peddinti <79261811+sujanchory@users.noreply.github.com>
2021-04-16 17:38:54 +0530

    Add files via upload

---
+++ Armstrong_number.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int temp,c,count=0,num,res=0,rem;scanf("%d",&num);
+    temp=num;
+    while(temp)
+     {
+       temp=temp/10;
+       count++;
+     }
+       temp=num;
+    while(temp)
+      {
+       rem=temp%10;
+       temp=temp/10;
+       res=res+pow(rem,c);
+       }
+    if(num==res)
+      {
+         printf("ArmStrong Number");
+      }
+    else
+      {
+         printf("Not an ArmStrong Number");
+      }
+}
