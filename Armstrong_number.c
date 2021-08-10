314a6696808778d1560d48c165d15b6538fa695e
Sujan Peddinti <sujanchory@gmail.com>
2021-08-10 05:48:26 +0530

    Update Armstrong_number.c

--- Armstrong_number.c
+++ Armstrong_number.c
@@ -1,27 +1,19 @@
-#include<stdio.h>
-#include<math.h>
-int main()
-{
-    int temp,c,count=0,num,res=0,rem;scanf("%d",&num);
-    temp=num;
-    while(temp)
-     {
-       temp=temp/10;
-       count++;
-     }
-       temp=num;
-    while(temp)
-      {
-       rem=temp%10;
-       temp=temp/10;
-       res=res+pow(rem,c);
-       }
-    if(num==res)
-      {
-         printf("ArmStrong Number");
-      }
-    else
-      {
-         printf("Not an ArmStrong Number");
-      }
-}
+#include<stdio.h>  
+ int main()    
+{    
+int n,r,sum=0,temp;    
+printf("enter the number=");    
+scanf("%d",&n);    
+temp=n;    
+while(n>0)    
+{    
+r=n%10;    
+sum=sum+(r*r*r);    
+n=n/10;    
+}    
+if(temp==sum)    
+printf("armstrong  number ");    
+else    
+printf("not armstrong number");    
+return 0;  
+}   
